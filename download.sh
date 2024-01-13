975852420c1cc37fed37ac99f7232b560a28c8b4
prin <hi@prin.studio>
2024-01-14 02:54:59 +0800

    feat: add compiled TeX files

---
+++ download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -x
+mkdir -p tex
+
+# Download neccessary files for TeX engine.
+if [ ! -d "./tikzjax" ]; then
+  git clone https://github.com/artisticat1/tikzjax.git
+fi
+
+cd ./tikzjax
+git switch output-single-file
+git pull
+cp ./tex.wasm.gz ./core.dump.gz ../tex
+
+mkdir -p ./dist
+cp -r ./tex_files ./dist
+cd ./dist/tex_files
+
+# Make a reproducible tarball.
+find . -name '*.gz' -type f -print -exec gzip -d {} \;
+TZ=UTC0 tar \
+  --sort=name \
+  --mtime='1970-01-01 00:00:00' \
+  --owner=0 \
+  --group=0 \
+  --numeric-owner \
+  -czvf ../../../tex/tex_files.tar.gz .
+
+cd ../../
+rm -rf ./dist
